5dd3c418725939a83534116eb857c928ef2e38f8
poe <zhpooer@gmail.com>
2013-07-25 21:56:20 +0800

    P02 P03

--- src/main/scala/P01T10.scala
+++ src/main/scala/P01T10.scala
@@ -5,9 +5,24 @@ import java.util.NoSuchElementException
 trait P01T10 {
   def last[T](seq:Seq[T]):T = {
     seq match {
-      case List(a) => a
+      case a::Nil => a
       case a::t => last(t)
       case _ => throw new NoSuchElementException
     }
   }
+
+  def penultimate[T](seq:Seq[T]):T = {
+    seq match {
+      case a::List(_) => a
+      case a::t => penultimate(t)
+      case _ => throw new NoSuchElementException
+    }
+  }
+
+  def nth[T](idx:Int, seq:Seq[T]):T = (idx, seq) match {
+    case (0, a::_) => a
+    case (n, a::b) => nth(n-1, b)
+    case _ =>  throw new NoSuchElementException
+  }
+
 }
--- src/test/scala/P01T10Spec.scala
+++ src/test/scala/P01T10Spec.scala
@@ -8,4 +8,13 @@ class P01T10Spec extends FlatSpec with ShouldMatchers {
   it should "find the last element of a list" in new P01T10{
     last(List(1, 1, 2, 3, 5, 8)) should equal (8)
   }
+
+  it should " find the last but one element of a list" in new P01T10{
+    penultimate(List(1, 1, 2, 3, 5, 8)) should equal (5)
+  }
+
+  it should "find the Kth element of a list" in new P01T10{
+    nth(2, List(1, 1, 2, 3, 5, 8)) should equal (2)
+  }
+  // it should "" in new P01T10{}
 }
